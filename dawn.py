66e99fb691f67992ec5d127891cf362a623f1faa
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-02-14 00:46:55 +0500

    Add Csv Dawn File

--- dawn.py
+++ dawn.py
@@ -1,4 +1,5 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 
@@ -20,3 +21,20 @@ for headline in headlines:
         print(text)
 
 
+with open("down_news.csv" , "w" , newline="" , encoding= "utf-8"  ) as file:
+    writer = csv.writer(file)
+
+    writer.writerow([["Title", "Link"]])
+
+
+    for h in headline:
+        link_tag = h.find('a')
+    
+            
+        if link_tag:
+            title = link_tag.get_text(strip=True)
+            link = link_tag.get("href")
+            
+            writer.writerow([title, link])
+
+print("Data successfully saved to dawn_news.csv")
